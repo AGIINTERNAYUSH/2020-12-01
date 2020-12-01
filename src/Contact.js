26a4d922d4c104ea308773f365278340e969dcb2
Ayush Amatya <ayush@mail.com>
2020-12-01 15:32:12 +0545

    completed contact page desgin

--- src/Contact.js
+++ src/Contact.js
@@ -1,7 +1,11 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import './contact.css';
-import {Grid} from '@material-ui/core';
+import {Divider, Grid} from '@material-ui/core';
+import facebook from '../src/img/facebook.png'
+import skype from '../src/img/skype.jpg'
+import instagram from '../src/img/instagram.jpg'
+import twitter from '../src/img/twitter.png'
 
 export default class Contact extends Component {
     constructor(props) {
@@ -56,8 +60,9 @@ export default class Contact extends Component {
     render(){
         return(
             <div className="container">
-                <Grid container spacing={2}>
-                    <Grid item xs={12} md={3}>
+                <Grid container spacing={10}>
+                    <Grid item xs={12} md={8}>
+                        <h1 style={{color:'darkblue'}}>Send review of our product:</h1>
                         <form onSubmit={this.onSubmit}>
                             <div className="form-group">
                                 <label style={{color:'#337ab7'}}>Name: </label>
@@ -68,13 +73,39 @@ export default class Contact extends Component {
                                 <input type="text" className="form-control" value={this.state.email} onChange={this.onChangeEmail} />
                             </div>
                             <div className="form-group">
+                                <label style={{color:'#337ab7'}}>Product Purchased: </label>
+                                <input type="text" className="form-control" value={this.state.product} onChange={this.onChangeProduct} />
+                            </div>
+                            <div className="form-group">
                                 <label style={{color:'#337ab7'}}>Review: </label>
-                                <input type="text" className="form-control" value={this.state.review} onChange={this.onChangeReview} />
+                                <textarea rows='5' type="textfield" className="form-control" value={this.state.review} onChange={this.onChangeReview} />
                             </div>
                             <div className="form-group">
                                 <input type="submit" value="Submit" className="btn btn-primary"/>
                             </div>
                         </form>
+                    </Grid>
+                    <Grid item xs={12} md={4} style={{textAlign:"center"}}>
+                        <div className="box2">
+                            <h4 style={{color:"darkblue"}}>You can also contact us through:</h4>
+                            <Divider/>
+                            <a href="https://www.facebook.com">
+                                <img src={facebook} className="githubIcon" alt="asdf" width="100px" height="70px" style={{align:"center"}}/>
+                            </a>
+                            <Divider/>
+                            <a href="https://www.instagram.com">
+                                <img src={instagram} className="githubIcon" alt="asdf" width="100px" height="70px" style={{align:"center"}}/>
+                            </a>
+                            <Divider/>
+                            <a href="https://www.twitter.com">
+                                <img src={twitter} className="githubIcon" alt="asdf" width="100px" height="70px" style={{align:"center"}}/>
+                            </a>
+                            <Divider/>
+                            <a href="https://www.skype.com">
+                                <img src={skype} className="githubIcon" alt="asdf" width="100px" height="70px" style={{align:"center"}}/>
+                            </a>
+                            <Divider/>
+                        </div>                        
                     </Grid>
                 </Grid>
                 
